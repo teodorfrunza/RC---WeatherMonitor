ea10be8d262170e02238410a8b935854bd8dc36b
Frunza Teodor <teodor.frunza@info.uaic.ro>
2017-12-27 16:49:00 +0200

    modul1 v2

---
+++ modul1.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+/* Merge cu tot cu tokenize 
+int main (){
+    FILE * f = NULL;
+    char * line = NULL;
+    size_t length = 0;
+    //int count = 1;
+    char vect[2];
+    char * user;
+    char * password;
+
+    f = fopen("/home/leafy/Retele/WeatherMonitor/login","r");
+    if (f == NULL) {
+        perror ("Eroare la deschiderea fisierului.\n");
+        return errno;
+    }
+
+    while (( getline(&line, &length, f)) != EOF ) {
+        //printf("Line %d : %s", count, line);
+        //count+=1;
+        user = strtok(line," ");
+        password = strtok(NULL," ");
+        //printf("Username : %s ", user);
+        //printf("Password: %s ", password);
+    }
+    
+    fclose(f);
+    if (line)
+        free(line);
+}
+*/
+
+int verif(char * username, char * password){
+    FILE * f = NULL;
+    char * line = NULL;
+    size_t length = 0;
+    int count = 1;
+    char vect[2];
+    char * user;
+    char * pass;
+
+    f = fopen("/home/leafy/Retele/WeatherMonitor/login","r");
+    if (f == NULL) {
+        perror ("Eroare la deschiderea fisierului.\n");
+        return errno;
+    }
+
+    while (( getline(&line, &length, f)) != EOF ) {
+        user = strtok(line," ");
+        pass = strtok(NULL," ");
+        if (strcmp(user,username) == 0 && strcmp(pass, password) == 10) return 1;
+        /* VERIFICARI
+         * printf("Line %d : %s", count, line);
+         * count+=1;
+         * printf("Username : %s ", user);
+         * printf("Password: %s ", pass);
+         * printf("l_u, %zu , l_p , %zu",strlen(user),strlen(pass));
+         * printf("Verif : %d %d \n",strcmp(user,username),strcmp(pass, password));
+         */
+    }
+    fclose(f);
+    return 0;
+}
+
+int verifUser(char * username){
+    FILE * f = NULL;
+    char * line = NULL;
+    size_t length = 0;
+    char * user;
+
+    f = fopen("/home/leafy/Retele/WeatherMonitor/login","r");
+    if (f == NULL) {
+        perror ("Eroare la deschiderea fisierului.\n");
+        return errno;
+    }
+
+    while (( getline(&line, &length, f)) != EOF ) {
+        user = strtok(line," ");
+        if (strcmp(user,username) == 0) return 1;
+    }
+    fclose(f);
+    return 0;
+}
+
+int creareCont(char * username, char * password){
+    FILE * f = NULL;
+    FILE * f2 = NULL;
+    char * line = NULL;
+    size_t length = 0;
+
+    f = fopen("/home/leafy/Retele/WeatherMonitor/login","a");
+    if (f == NULL) {
+        perror ("Eroare la deschiderea fisierului.\n");
+        return errno;
+    }
+    
+    if(verifUser(username) == 1) return 0;
+
+    char instr[100] = "touch /home/leafy/Retele/WeatherMonitor/userfiles/";
+    strcat(instr,username);
+    system(instr);
+
+    fprintf(f,"%s %s\n",username,password);
+    fclose(f);
+    return 1;
+}
+
+
+
+int main(void){
+    char * username = "root";
+    char * password = "toor";
+    char * username2 = "vasile";
+    char * password2 = "tuica";
+    printf("Verif %d ",verif(username,password));
+    printf("V_u %d ",verifUser(username));
+    printf("V_u %d ",verifUser(username2));
+    printf("Creare %d ",creareCont(username2,password2));
+    
+}
+
